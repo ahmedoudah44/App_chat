c3235a0ae3c03df76651c1daa19a3f9a399ec2c3
Ahmedou dah <ahmedoudah44@gmail.com>
2017-12-23 21:05:29 +0100

    Database class

--- src/mr/iscae/app_Chat/DataBase/DataBaseClass.java
+++ src/mr/iscae/app_Chat/DataBase/DataBaseClass.java
@@ -1,7 +1,26 @@
 package mr.iscae.app_Chat.DataBase;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import mr.iscae.app_Chat.model.*;
+import sun.rmi.runtime.Log;
+
+
+
+
 
 public class DataBaseClass {
 
+	private static Map<Log, Message> messages= new HashMap<>();
+	private static Map<Log, Profile> profiles= new HashMap<>();
 	
+	public static Map<Log, Message> getMessages(){
+		return messages;
+	}
+	
+	public static Map<Log, Profile>  getProfile(){
+		
+		return profiles;
+	}
 }
