52c19e213cfa4fab369813e074858e900752bff8
Ahmedou dah <ahmedoudah44@gmail.com>
2017-12-23 20:49:57 +0100

    class Prifile model

---
+++ src/mr/iscae/app_Chat/DataBase/DataBaseClass.java
@@ -0,0 +1,7 @@
+package mr.iscae.app_Chat.DataBase;
+
+
+public class DataBaseClass {
+
+	
+}
---
+++ src/mr/iscae/app_Chat/model/Profile.java
@@ -0,0 +1,54 @@
+package mr.iscae.app_Chat.model;
+
+import java.util.Date;
+
+public class Profile {
+
+	private long id;
+	private String proFileNom;
+	private String nom;
+	private String email;
+	private Date creatdate;
+	public Profile() {
+		// TODO Auto-generated constructor stub
+	}
+	public Profile(long id, String proFileNom, String nom, String email, Date creatdate) {
+		super();
+		this.id = id;
+		this.proFileNom = proFileNom;
+		this.nom = nom;
+		this.email = email;
+		this.creatdate = creatdate;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getProFileNom() {
+		return proFileNom;
+	}
+	public void setProFileNom(String proFileNom) {
+		this.proFileNom = proFileNom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreatdate() {
+		return creatdate;
+	}
+	public void setCreatdate(Date creatdate) {
+		this.creatdate = creatdate;
+	} 
+	
+}
